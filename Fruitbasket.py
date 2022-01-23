3b335192d3ce72dd61128a4c7277f7091b6a0c43
Nikhil <92679171+knikhil25@users.noreply.github.com>
2022-01-23 14:32:37 +0530

    Try it out on:
    https://colab.research.google.com/?utm_source=scs-index

---
+++ Fruitbasket.py
@@ -0,0 +1,18 @@
+fruit = input("Enter all the fruits you like!(I hope you put:Apple,Banana,Strawberry,Blueberry,Grape ): ")
+
+if fruit=="Apple":
+  print ("An Apple a day keeps the docter away") 
+elif fruit=="Banana":
+  print ("After eating Banana you become Nana!")
+elif fruit=="Strawberry":
+  print ("Make Strawberry juice and drink with a Starwberry floaverd straw!")
+elif fruit=="Blueberry":
+  print ("After eating Blueberry don't feel blue!")
+elif fruit=="Orange":
+  print ("Are you talking about the fruit or the color?")
+elif fruit=="Grape":
+  print ("After eating Grape you drape!")
+elif fruit=="Apple,Banana,Strawberry,Blueberry,Grape":
+  print ("Looks like you Like most of the fruits!")
+else:
+  print ("Uh-oh looks like you do not like most of the fruits!")
