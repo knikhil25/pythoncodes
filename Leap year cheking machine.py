6d2a2414be9cc8f38189ab91f1d8ab78196508d6
Nikhil <92679171+knikhil25@users.noreply.github.com>
2022-01-26 12:22:06 +0530

    Corrected it!

--- Leap year cheking machine.py
+++ Leap year cheking machine.py
@@ -9,8 +9,8 @@ if (year % 400 == 0) and (year % 100 == 0):
 # not divided by 100 means not a century year
 # year divided by 4 is a leap year
 elif (year % 4 ==0) and (year % 100 != 0):
-  print ("2021 is a leap year".format(year))
+  print ("{0} is a leap year".format(year))
 # if not divided by both 400 (century year) and 4 (not century year)
 # year is not leap year
 else:
-  print ("2021 is not a leap year")
+  print ("{0} is not a leap year".format(year))
