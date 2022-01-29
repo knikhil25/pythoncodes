86fbd3f2fdbb886d7eb505b5ef3a06a1063118bf
Nikhil <92679171+knikhil25@users.noreply.github.com>
2022-01-29 11:52:10 +0530

    Update Shuffle deck of cards.py

--- Shuffle deck of cards.py
+++ Shuffle deck of cards.py
@@ -2,9 +2,9 @@
 # importing modules
 import itertools, random
 # make a deck of cards
-deck = list(itertools.product(range(1,14)['Spade','Heart','Diamond','Club']))
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 # shuffle the cards
-random.shuffule(deck)
+random.shuffle(deck)
 # draw five cards
 print("You got: ")
 for i in range(5):
