3e31386f276d8abad58c317e3f93cd87d9eb03d9
Nikhil <92679171+knikhil25@users.noreply.github.com>
2025-03-02 16:48:23 -0800

    Update Band_name_generator.py

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,6 +1,6 @@
 city_name = input("What is your city name? ")
 while city_name == "":
-    city_name = input("What is your city name? ") 
+    city_name = input("What is your city name? Please wentr something") 
 pets_name = input("what is your pets name? ")
 if pets_name == "None":
     user_name = input("What is your name? ")
