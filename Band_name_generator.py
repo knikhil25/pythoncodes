16867738430f6e45efa9fcb5bd32e69d28e62912
Nikhil <92679171+knikhil25@users.noreply.github.com>
2025-03-02 16:50:24 -0800

    Update Band_name_generator.py

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,11 +1,11 @@
 city_name = input("What is your city name? ")
 while city_name == "":
     city_name = input("What is your city name? Please enter something! ") 
-pets_name = input("what is your pets name? ")
+pets_name = input("what is your pets name?: ")
 if pets_name == "None":
-    user_name = input("What is your name? ")
+    user_name = input("What is your name?: ")
 elif pets_name == "none":
-    user_name = input("What is your name? ")
+    user_name = input("What is your name?: ")
     print("Your band name should be: " + user_name + " from " + city_name + "'s band")
 elif pets_name == "" :
     print("Your band should be named: The " + city_name + "kers")
